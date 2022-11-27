3f69ba435a0864099ff9ea594bbe023d2a476640
Artem Grokha <grokha.artem@gmail.com>
2022-11-27 22:07:07 +0200

    update

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const filters = 'name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?fields=${filters}`).then(response => {
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
+    return response.json();
+  });
+}
